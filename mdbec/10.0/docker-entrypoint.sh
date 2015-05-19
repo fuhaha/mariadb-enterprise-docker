8fef6728f63dc4e44e3c7c7dbb48f82c317ce223
Ilya V. Storozhilov <Ilya_Storozhilov@epam.com>
2015-05-19 17:01:59 +0300

    MDBEC Docker image prototype has been implemented and tested

--- mdbec/10.0/docker-entrypoint.sh
+++ mdbec/10.0/docker-entrypoint.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 #
 # MariaDB Enterprise Galera Cluster Docker image entry point
+#
+# TODO: SST_AUTH environment variable support for setting SST credentials in my.cnf
 
 set -e
 
@@ -11,26 +13,24 @@ if [ "${1:0:1}" = '-' ] ; then
 fi
 
 if [ "$1" = "${mysqld}" ]; then
+	# Initializing database directory if needed
+	if [ ! -e /var/lib/mysql/bootstrapped ]; then
+		status "Initializing MariaDB database in '/var/lib/mysql' directory"
+		mysql_install_db
+		touch /var/lib/mysql/bootstrapped
+	fi
+
+	# Composing start command
 	if [ -z "${CLUSTER+x}" ]; then
-		echo "CLUSTER environment variable must be set to 'BOOT', 'INIT' or comma-separated cluster node list" >&2
-		exit 1
-	elif [ ${CLUSTER} = "BOOT" ]; then
 		NODE=1 # force this
-		# grab the ip address of eth0 if NODE_ADDR is not provided 
-		NODE_ADDR=${NODE_ADDR:-$(ip -4 addr show eth0 | grep inet | sed -e 's/  */ /g' | cut -d ' ' -f 3 | cut -d '/' -f 1)}
-		#set -- "$@" --wsrep_node_address="${NODE_ADDR}" \
-		#	--wsrep_node_incoming_address="${NODE_ADDR}" \
-		#	--wsrep_new_cluster --wsrep_cluster_address="gcomm://" \
-		#	--wsrep_provider_options="$WSREP_PROVIDER_OPTIONS" \
-		#	--log-error=/var/lib/mysql/mysql-${NODE}.error.log
 		set -- "$@" --wsrep_new_cluster --wsrep_cluster_address="gcomm://" \
 			--log-error=/var/lib/mysql/mysql-${NODE}.error.log
 		if [ ! -z "${MARIADB_ROOT_PASSWORD}" ] ; then
-			temp_sql_file='/tmp/mysql-first-time.sql'
+			temp_sql_file='/tmp/mysql-set-root-password.sql'
 			cat > "${temp_sql_file}" <<-EOSQL
 				DELETE FROM mysql.user WHERE user='root';
 				CREATE USER 'root'@'%' IDENTIFIED BY '${MARIADB_ROOT_PASSWORD}';
-				GRANT ALL ON *.* TO 'root'@'%' WITH GRANT OPTION;
+				GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' WITH GRANT OPTION;
 				FLUSH PRIVILEGES;
 			EOSQL
 			set -- "$@" --init-file="${temp_sql_file}"
@@ -40,25 +40,9 @@ if [ "$1" = "${mysqld}" ]; then
 			echo "NODE environment variable must be set to node number" >&2
 			exit 1
 		fi
-		NODE_ADDR=${NODE_ADDR:-$(ip -4 addr show eth0 | grep inet | sed -e 's/  */ /g' | cut -d ' ' -f 3 | cut -d '/' -f 1)}
-		set -- "$@" --wsrep_node_address="${NODE_ADDR}" \
-			--wsrep_node_incoming_address="${NODE_ADDR}" \
-			--wsrep_provider_options="$WSREP_PROVIDER_OPTIONS" \
-			--wsrep_cluster_address="gcomm://${CLUSTER}" --log-error=/var/lib/mysql/mysql-${NODE}.error.log
+		set -- "$@" --wsrep_cluster_address="gcomm://${CLUSTER}" \
+			--log-error=/var/lib/mysql/mysql-${NODE}.error.log
 	fi
-#	if [ -z "${MARIADB_ROOT_PASSWORD}" ] ; then
-#		echo >&2 "ERROR: MariaDB root user password not set. Did you forget to add '-e MARIADB_ROOT_PASSWORD=...'?"
-#		exit 1
-#	fi
-#	temp_sql_file='/tmp/mysql-first-time.sql'
-#	cat > "${temp_sql_file}" <<-EOSQL
-#		DELETE FROM mysql.user WHERE user='root';
-#		CREATE USER 'root'@'%' IDENTIFIED BY '${MARIADB_ROOT_PASSWORD}';
-#		GRANT ALL ON *.* TO 'root'@'%' WITH GRANT OPTION;
-#		FLUSH PRIVILEGES;
-#	EOSQL
-#	set -- "$@" --init-file="${temp_sql_file}"
 fi
 
-echo "$(date --rfc-3339=ns): executing '$@' command" >> /exec_cmd.log
 exec "$@"
